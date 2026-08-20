dda71ce424f387fc5d6c5b1f613630bf7b275ff9
Alayaki6 <150488587+Alayaki6@users.noreply.github.com>
2026-08-20 15:00:54 +0100

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,46 @@
-function Navbar() {
+function Navbar({
+  user,
+  onJobs,
+  onLogin,
+  onRegister,
+  onLogout,
+}) {
   return (
     <nav className="navbar">
-      <div className="navbar-brand">
+      <button
+        type="button"
+        className="navbar-brand"
+        onClick={onJobs}
+      >
         JobPilot AI
-      </div>
+      </button>
 
       <div className="navbar-links">
-        <button type="button">Jobs</button>
-        <button type="button">Saved Jobs</button>
-        <button type="button">Login</button>
+        <button type="button" onClick={onJobs}>
+          Jobs
+        </button>
+
+        {user ? (
+          <>
+            <span className="navbar-user">
+              Hi, {user.username}
+            </span>
+
+            <button type="button" onClick={onLogout}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <button type="button" onClick={onLogin}>
+              Login
+            </button>
+
+            <button type="button" onClick={onRegister}>
+              Sign Up
+            </button>
+          </>
+        )}
       </div>
     </nav>
   );
