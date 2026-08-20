2e3184a63efdf50e095a0987f92f1af0d444fb24
Alayaki6 <150488587+Alayaki6@users.noreply.github.com>
2026-08-20 13:08:21 +0100

    Create serializers.py

---
+++ backend/jobs/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+
+from .models import Job
+
+
+class JobSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Job
+        fields = [
+            "id",
+            "title",
+            "company",
+            "location",
+            "description",
+            "employment_type",
+            "salary",
+            "application_url",
+            "is_active",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = [
+            "id",
+            "created_at",
+            "updated_at",
+        ]
