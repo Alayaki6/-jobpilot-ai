6beff7c6fe62cf27c82666354c36d7b70845f6a2
Alayaki6 <150488587+Alayaki6@users.noreply.github.com>
2026-08-20 13:07:15 +0100

    Delete backend/views.py

--- backend/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import generics
-
-from .models import Job
-from .serializers import JobSerializer
-
-
-class JobListCreateView(generics.ListCreateAPIView):
-    queryset = Job.objects.filter(is_active=True)
-    serializer_class = JobSerializer
